a276793d6a71d6e7f37b2d382ce654951f8693c7
bay007 <tnt.galicia@gmail.com>
2016-11-28 01:51:54 -0600

    Bindable Members Up Top

--- Book.controller.js
+++ Book.controller.js
@@ -1,12 +1,14 @@
 (function(){
-	
+
 	'use strict';
 	angular
 	.module( 'helloApp' )
 
-	.controller( 'BookController', [ 'Libro', '$scope', '$http', BookController ]);
+	.controller( 'BookController', cBookController );
 
-	function BookController( Libro, $scope, $http ) {
+	cBookController.$inject=['Libro', '$scope', '$http'];
+	
+	function cBookController( Libro, $scope, $http ) {
 		  $scope.book = new Libro();   	//This way all book’s behavior is encapsulated in Book service. Now, let’s use our shiny Book 
 		  $scope.book.load();			//service in our BookController.
 	};
--- Libro.factory.js
+++ Libro.factory.js
@@ -3,41 +3,53 @@
 	angular
 	.module( 'helloApp' )
 
-	.factory( 'Libro',[ '$http', Libro]);
+	.factory( 'Libro',fLibro);
+	
+	fLibro.$inject=['$http'];
+	
+	function fLibro( $http ){
+	
+		function Libro(){};		//As you can see, the controller became very thin. It now creates a Book instance, assigns it
+								//to the scope and loads it from the backend. When the book will be loaded, it’s properties 							
+								//will be changed and so the template. Keep in mind that other controllers that interact with
+								//a book, simply inject the Book service.
+		function setData( _jsonBookData ){
+			angular.extend( this,_jsonBookData );
+		};
 
-	function Libro( $http ){
-			
-			function Libro(){};		//As you can see, the controller became very thin. It now creates a Book instance, assigns it
-									//to the scope and loads it from the backend. When the book will be loaded, it’s properties 							//will be changed and so the template. Keep in mind that other controllers that interact with 								//a book, simply inject the Book service.
-			
-			Libro.prototype = {
-				load:function(){
-					var url = 'http://www.mocky.io/v2/583664bb1100006b190c00a1?callback=JSON_CALLBACK';
-					var _scope=this;
-					$http.jsonp( url ).success( function( jsonBookData ) {
-						_scope.setData( jsonBookData );
-					});
+		function load(){
+			var url = 'http://www.mocky.io/v2/583664bb1100006b190c00a1?callback=JSON_CALLBACK';
+			var _scope=this;
+			$http.jsonp( url ).success( function( jsonBookData ) {
+				_scope.setData( jsonBookData );
+			});
+		};
 
-				},
-				setData:function( _jsonBookData ){
-					angular.extend( this,_jsonBookData );
-				},
-				delete:function(){
-					alert( "Se ha invocado el borrado" );
-				},
-				update:function(){
-					alert( "Se ha invocado el Update" );
-				},
-				isAvailable:function(){
-					if ( !this.stores || this.stores.length === 0 ) {
-						return false;
-					}
+		function remove(){
+			alert( "Se ha invocado el borrado" );
+		};
 
-					return ( this.stores.length>0 );
-				}
+		function update(){
+			alert( "Se ha invocado el Update" );
+		};
 
-			};
+		function isAvailable(){
+			if ( !this.stores || this.stores.length === 0 ) {
+				return false;
+			}
+			return ( this.stores.length>0 );
+		};
 
-			return Libro;
+
+		Libro.prototype = {
+			load: load,
+			setData:setData,
+			remove:remove,
+			update:update,
+			isAvailable:isAvailable
+		};
+
+		return Libro;
 	};
+
 })();
